7c0f9ee78872a933a5ebf6d762b236b38948dad6
rajesh.paul <rajesh.paul@acquia.com>
2026-01-27 18:25:16 +0530

    Metrics query changes in sumologic tap

--- tap_sumologic/streams.py
+++ tap_sumologic/streams.py
@@ -264,6 +264,7 @@ class SearchJobStream(SumoLogicStream):
                 metrics_data = response["queryResult"][0]["timeSeriesList"][
                     "timeSeries"
                 ]
+                raise RuntimeError("METRIC DATA: %s", metrics_data)
                 self.logger.info(
                     f"✓ Retrieved {len(metrics_data)} time series from Sumo Logic"
                 )
--- tap_sumologic/sumologic_sdk.py
+++ tap_sumologic/sumologic_sdk.py
@@ -156,8 +156,6 @@ class SumoLogic(object):
                 },
             },
         }
-        self.logger.info("params for metrics query: %s", params)
-
         if quantization is None:
             del params["queries"][0]["quantization"]
         if rollup is None:
@@ -165,8 +163,6 @@ class SumoLogic(object):
         if timeshift is None:
             del params["queries"][0]["timeshift"]
         r = self.post("/metricsQueries", params)
-
-        self.logger.info("API response r: %s", r.text)
         return json.loads(r.text)
 
     def get_sumologic_fields(
